bc9df245a49e3dae048cf3ef310f2a8b828a6d9c
MartinMWilliams <31654354+MartinMWilliams@users.noreply.github.com>
2017-12-05 10:18:37 -0800

    whoops

--- FinalProject/FinalProject/Controllers/ReservationViewModel.cs
+++ FinalProject/FinalProject/Controllers/ReservationViewModel.cs
@@ -7,6 +7,7 @@ namespace FinalProject.Controllers
 {
     public class ReservationViewModel
     {
+        public Int32 ReservationNumber { get; set; }
         public bool RoundTrip { get; set; }
 
         public bool AnotherFlight { get; set; }
