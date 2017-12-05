ed0ccc3121da75386e9467f91c68dc644821e833
MartinMWilliams <31654354+MartinMWilliams@users.noreply.github.com>
2017-12-05 10:17:07 -0800

    added reservation viewmodel

---
+++ FinalProject/FinalProject/Controllers/ReservationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Controllers
+{
+    public class ReservationViewModel
+    {
+        public bool RoundTrip { get; set; }
+
+        public bool AnotherFlight { get; set; }
+
+        public String DepartureCity { get; set; }
+
+        public String ArrivalCity { get; set; }
+
+        public Int32 NumberOfFliers { get; set; }
+
+        public DateTime DepartureDay { get; set; }
+    }
+}
